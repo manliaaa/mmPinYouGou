01ae8f744c745f3e99f8e9befe2e2c9104001d6a
ManMan <870222569@qq.com>
2019-01-09 11:49:00 +0800

    提交得是老师的代码

---
+++ interface/src/main/java/cn/itcast/core/service/OrderService.java
@@ -0,0 +1,10 @@
+package cn.itcast.core.service;
+
+import cn.itcast.core.pojo.entity.Result;
+import cn.itcast.core.pojo.order.Order;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface OrderService {
+
+    public void add(Order order);
+}
